4b364dfbfbf441c4282dc33a612568dd56459825
irajgreenberg <irajgreenberg@gmail.com>
2024-04-06 11:42:01 -0600

    hopefully adding three templat

---
+++ .fttemplates/three_Template/[FTName].ts
@@ -0,0 +1,26 @@
+// <FTName>
+// Ira Greenberg
+// Santa Fe, NM | Dallas, TX
+// [DATE_NOW('yyyy')]
+
+// Class Description: 
+
+import { Group, Vector3 } from "three";
+import { randFloat, randInt } from 'three/src/math/MathUtils';
+import { FuncType, saveImage, PI, TWO_PI, sin, cos } from "../../libPByte_3/IJGUtils";
+
+export class <FTName> extends Group {
+
+    constructor() {
+        super();
+        this.create();
+    }
+
+    create() {
+    }
+
+    move(time: number) {
+    }
+}
+
+
