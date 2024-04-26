1776f51532b88115c01ea2a53f68476d060b7204
Greenberg <irajgreenberg@gmail.com>
2024-04-26 10:39:20 -0600

    adding layered single one

---
+++ src/three/Projects/ProtoMorphogenesis_Single/ProtoOrganism_Single.ts
@@ -0,0 +1,222 @@
+// ProtoMorphogenesis
+// Ira Greenberg
+// Santa Fe, NM | Dallas, TX
+// 2024
+
+/* Class Description: 
+Class encapsulates a VerletSurface with optionally attached tendrils
+*/
+
+import { CatmullRomCurve3, Color, Curve, CurvePath, DoubleSide, Group, Material, Mesh, MeshBasicMaterial, TubeGeometry, Vector2, Vector3 } from "three";
+import { randFloat, randInt } from 'three/src/math/MathUtils';
+import { FuncType, saveImage, PI, TWO_PI, sin, cos, AnchorPoint, SimplCurve } from "../../libPByte_3/IJGUtils";
+import { VerletStrand } from "../../libPByte_3/VerletStrand";
+import { VerletStick } from "../../libPByte_3/VerletStick";
+import { VerletNode } from "../../libPByte_3/VerletNode";
+import { TendrilDataModel } from "../../libPByte_3/TendrilDataModel";
+import { VerletSurface } from "../../libPByte_3/VerletSurface";
+import { ProtoPhysics } from "../../libPByte_3/ProtoPhysics";
+
+
+/**
+ * Used ONLY be this class so didn't want to put into
+ * IJGUtils.
+ */
+export enum NodeSelector {
+    TargetNode = 'targetNode',
+    TargetNodes = 'targetNodes',
+    Centroid = 'centroid',
+    SingleRandom = 'singleRandom',
+    MultipleRandom = 'multipleRandom',
+    All = 'all',
+}
+
+export class ProtoOrganism_Single extends Group {
+
+    pos: Vector3;
+    body: VerletSurface;
+    tdm?: TendrilDataModel;
+    col: Color;
+    physics: ProtoPhysics;
+
+    tendrils: VerletStrand[] = [];
+    tendrilSticks: Mesh[] = [];
+    tendrilStickRadii: number[] = [];
+
+    private substructureZIndex: number = 0;
+    private substructureZIndexDepth: number = 30;
+
+
+    constructor(pos: Vector3, body: VerletSurface, col: Color, physics: ProtoPhysics, tdm?: TendrilDataModel) {
+        super();
+        this.pos = pos;
+        this.body = body;
+        this.col = col;
+        this.physics = physics;
+        this.add(this.body);
+
+        if (tdm) {
+            this.tdm = tdm;
+            this.create();
+        }
+
+    }
+
+    create() {
+        //create tendrils
+        if (this.tdm) {
+            for (let i = 0; i < this.body.edgeNodes.length; i++) {
+                const head = this.body.edgeNodes[i].position;
+                const tail = new Vector3().copy(this.body.edgeNodes[i].position).multiplyScalar(this.tdm.length);
+                this.tendrils.push(new VerletStrand(head, tail, 6, AnchorPoint.HEAD, .3));
+                //  this.add(this.tendrils[this.tendrils.length - 1]);
+
+                //   this.tendrils[i].setStrandColor(new Color("0xff6666"));
+                this.tendrils[i].nodes[0].isVerletable = false;
+
+
+                const path = new CatmullRomCurve3(this.tendrils[i].getNodeVecs());
+                this.tendrilStickRadii.push(randFloat(this.tdm.radiiMinMax.x, this.tdm.radiiMinMax.y));
+                const geometry = new TubeGeometry(path, this.tdm.segments, randFloat(this.tdm.radiiMinMax.x, this.tdm.radiiMinMax.y), randInt(this.tdm.radialSegsmentsMinMax.x, this.tdm.radialSegsmentsMinMax.y), false);
+                const material = new MeshBasicMaterial({ color: this.col, side: DoubleSide, transparent: true, opacity: .5 });
+                const mesh = new Mesh(geometry, material);
+                this.tendrilSticks.push(new Mesh(geometry, material));
+                this.add(this.tendrilSticks[this.tendrilSticks.length - 1]);
+            }
+        }
+        // //this.body.getEdgeVecs();
+        // this.position.x += this.pos.x
+        // this.position.y += this.pos.y
+        // this.position.z += this.pos.z
+    }
+
+    /**
+     * Enum Options:
+     * TargetNode = 'targetNode',
+     * TargetNodes = 'targetNodes',
+     * Centroid = 'centroid',
+     * SingleRandom = 'singleRandom',
+     * MultipleRandom = 'multipleRandom',
+     * All = 'all'
+     * 
+     * note: NodeID only used for targetnode/s options
+     */
+    start(node: NodeSelector, vec: Vector3, nodeID?: number | number[], randintCount?: number): void {
+        if (node === 'centroid') {
+            this.body.centroidNode.moveNode(vec);
+        } else if (node === 'targetNode') {
+            if (typeof nodeID == 'number') {
+                this.body.bodyNodes[nodeID].moveNode(vec);
+            }
+        } else if (node === 'targetNodes') {
+            if (nodeID instanceof Array) {
+                for (let i = 0; i < nodeID.length; i++) {
+                    this.body.bodyNodes[nodeID[i]].moveNode(vec);
+                }
+            }
+        } else if (node === 'singleRandom') {
+            const n = randInt(0, this.body.bodyNodes.length - 1);
+            this.body.bodyNodes[n].moveNode(vec);
+        } else if (node === 'multipleRandom') {
+            if (randintCount !== undefined) {
+                for (let i = 0; i < randintCount; i++) {
+                    const n = randInt(0, this.body.bodyNodes.length - 1);
+                    this.body.bodyNodes[n].moveNode(vec);
+                }
+            }
+        } else if (node === 'all') {
+            for (let i = 0; i < this.body.bodyNodes.length; i++) {
+                this.body.bodyNodes[i].moveNode(vec);
+            }
+        }
+    }
+
+    public setSurfaceDrawable(areNodesDrawable: boolean = false, areSticksDrawable: boolean = false, areCrossSupportsDrawable: boolean = false): void {
+        this.body.setDrawable(areNodesDrawable, areSticksDrawable, areCrossSupportsDrawable);
+    }
+
+    private _update(): void {
+        this.body.update();
+
+        if (this.tdm) {
+            for (let i = 0; i < this.tendrils.length; i++) {
+                let input = new Vector3().copy(this.body.edgeNodes[i].position).multiplyScalar(1);
+                this.tendrils[i].setHeadPosition(input);
+                this.tendrils[i].nodes[this.tendrils[i].nodes.length - 1].position.multiplyScalar(randFloat(1.00005, 1.0004));
+                const path = new CatmullRomCurve3(this.tendrils[i].getNodeVecs());
+                this.tendrilSticks[i].geometry.dispose();
+                const geometry = new TubeGeometry(path, this.tdm.segments, this.tendrilStickRadii[i], 12, false);
+                this.tendrilSticks[i].geometry = geometry
+            }
+        }
+
+    }
+
+    verlet(): void {
+        this.body.verlet();
+        // move tendrils based on VerletSurface
+        this._update();
+        for (let i of this.tendrils) {
+            i.verlet();
+        }
+    }
+
+
+    /**
+    * just centroid for now
+    */
+    pulse(): void {
+        this.body.centroidNode.position.z = sin(this.physics.theta) * this.physics.amp;
+        this.physics.theta += this.physics.freq;
+    }
+
+    /**
+    * Randomly jiggle all bodynodes
+    * with optional fixed passed values
+    */
+    jitter(vecMax?: Vector3 | Vector3[]): void {
+        if (vecMax instanceof Vector3) {
+            for (let i = 0; i < this.body.nodes.length; i++) {
+                const v = new Vector3(randFloat(-vecMax.x, vecMax.x), randFloat(-vecMax.y, vecMax.y), randFloat(-vecMax.z, vecMax.z));
+                this.body.nodes[i].position.add(v);
+            }
+        } else {
+
+        }
+    }
+
+    move(spd?: Vector2 | Vector3): void {
+        if (spd instanceof Vector3) {
+            this.position.x = this.pos.x + spd.x;
+            this.position.y = this.pos.y + spd.y;
+            this.position.z = this.pos.z + spd.z;
+        } else if (spd instanceof Vector2) {
+            this.position.x += this.physics.spd.x;
+            this.position.y += this.physics.spd.y;
+        }
+    }
+
+    rotate(rotSpd: Vector3): void {
+        this.rotateX(rotSpd.x);
+        this.rotateY(rotSpd.y);
+        this.rotateZ(rotSpd.z);
+    }
+
+    addSubStructure(substructure: VerletSurface): void {
+        // increase with each substructure addition
+        this.substructureZIndex++;
+        // add to scenegraph
+        this.add(substructure);
+        substructure.position.x = this.position.x;
+        substructure.position.y = this.position.y;
+        substructure.position.x = this.position.z + this.substructureZIndexDepth * this.substructureZIndex;
+
+    }
+
+    setZIndexDepth(depth: number) {
+        this.substructureZIndexDepth = depth;
+
+    }
+}
+
+
---
+++ src/three/Projects/ProtoMorphogenesis_Single/sketch.ts
@@ -0,0 +1,221 @@
+// ProtoMorphogenesis
+// Ira Greenberg
+// Santa Fe, NM | Dallas, TX
+// 2024
+
+/* Project Description: 
+*/
+
+import { AmbientLight, Color, DirectionalLight, DoubleSide, FogExp2, HemisphereLight, MeshBasicMaterial, MeshPhongMaterial, PCFSoftShadowMap, PerspectiveCamera, PointLight, Scene, SpotLight, Texture, TextureLoader, Vector2, Vector3, WebGLRenderer } from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { randFloat, randInt } from 'three/src/math/MathUtils';
+import { FuncType, saveImage, PI, TWO_PI, sin, cos, AnchorPlane } from "../../libPByte_3/IJGUtils";
+import { NodeSelector, ProtoOrganism_Single } from './ProtoOrganism_Single';
+import { VerletPlane2 } from '../../libPByte_3/VerletPlane2';
+import { VerletSurface } from '../../libPByte_3/VerletSurface';
+import { TendrilDataModel } from '../../libPByte_3/TendrilDataModel';
+import { ProtoPhysics } from '../../libPByte_3/ProtoPhysics';
+import { ProtoPlasm } from './ProtoPlasm';
+
+// enum HasTendrils {
+//     NO,
+//     EDGES,
+//     EDGES_INNER,
+//     CENTER
+// }
+
+// create and position camera
+const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.001, 10000);
+camera.position.x = 0;
+camera.position.y = 0;
+camera.position.z = 400;
+
+const scene = new Scene();
+
+// dark
+let greyColR = randFloat(.01, .05);
+let greyColG = randFloat(.01, .05);
+let greyColB = randFloat(.01, .05);
+
+//light
+// let greyColR = randFloat(.4, .8);
+// let greyColG = randFloat(.4, .8);
+// let greyColB = randFloat(.4, .8);
+
+let colVal = (greyColR + greyColG + greyColB) / 3
+const myColor = new Color(greyColR, greyColG, greyColB);
+scene.background = myColor;
+document.body.style.backgroundColor = '#' + myColor.getHexString();
+let fogFactor = 0.0001;
+scene.fog = new FogExp2('#' + myColor.getHexString(), fogFactor)
+//scene.fog = new FogExp2(0x8888AA, fogFactor)
+
+// main renderer
+let renderer = new WebGLRenderer({ alpha: true, antialias: true, logarithmicDepthBuffer: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
+
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = PCFSoftShadowMap;
+document.body.appendChild(renderer.domElement);
+document.title = "[Proto]morphogenesis_005] | Ira Greenberg.2024"
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+/************************Custom code*************************/
+
+interface ImageMap {
+    imageStr: string;
+    hasTendrils: boolean;
+}
+
+const imageMaps: ImageMap[] = [
+    { imageStr: "Proto_Org_001.png", hasTendrils: false },
+    { imageStr: "Proto_Org_002.png", hasTendrils: false },
+    { imageStr: "Proto_Org_003.png", hasTendrils: false },
+    { imageStr: "Proto_Org_004.png", hasTendrils: false },
+    { imageStr: "Proto_Org_005.png", hasTendrils: false },
+    { imageStr: "Proto_Org_006.png", hasTendrils: false },
+    { imageStr: "Proto_Org_007.png", hasTendrils: true },
+    { imageStr: "Proto_Org_008.png", hasTendrils: false },
+    { imageStr: "Proto_Org_009.png", hasTendrils: false },
+    { imageStr: "Proto_Org_010.png", hasTendrils: false },
+    { imageStr: "Proto_Org_011.png", hasTendrils: false },
+    { imageStr: "Proto_Org_012.png", hasTendrils: false },
+    { imageStr: "Proto_Org_013.png", hasTendrils: false },
+    { imageStr: "Proto_Org_014.png", hasTendrils: false },
+    { imageStr: "Proto_Org_015.png", hasTendrils: false },
+    { imageStr: "Proto_Org_016.png", hasTendrils: false },
+    { imageStr: "Proto_Org_017.png", hasTendrils: true },
+    { imageStr: "Proto_Org_018.png", hasTendrils: false },
+    { imageStr: "Proto_Org_019.png", hasTendrils: false },
+    { imageStr: "Proto_Org_020.png", hasTendrils: true },
+    { imageStr: "Proto_Org_021.png", hasTendrils: true },
+    { imageStr: "Proto_Org_022.png", hasTendrils: true }
+];
+
+const protoOrgs: ProtoOrganism_Single[] = [];
+
+for (let i = 0; i < imageMaps.length; i++) {
+    const texture = new TextureLoader().load('data/ProtoMorph/' + imageMaps[i].imageStr);
+    const mat = new MeshBasicMaterial({ color: 0xffffff, transparent: true, wireframe: false, opacity: randFloat(.35, .7), side: DoubleSide, map: texture });
+    //mat.depthWrite = false;
+    // const mat = new MeshPhongMaterial({ color: 0xffffff, transparent: true, flatShading: false, wireframe: false, opacity: randFloat(.95, .99), emissive: 0xffffff, specular: 0xffffff, shininess: 30, side: DoubleSide, map: texture })
+    const sz = randFloat(170, 280);
+    const detail01 = randInt(16, 24);
+    const detail02 = randInt(8, 12);
+    const vSurf = new VerletSurface(new Vector2(sz, sz), new Vector2(detail01, detail02), mat, randFloat(.1, .8), true);
+    const pPhys = new ProtoPhysics(
+        randFloat(10, 15), //amp
+        PI / randFloat(20, 90), //freq
+        new Vector2(randFloat(-.35, .35), randFloat(-.35, .35)), //spd
+        new Vector3(randFloat(PI / -1200, PI / 1200), randFloat(PI / -1200, PI / 1200), randFloat(PI / -900, PI / 900)) //rotSpd
+    );
+    const tdm = new TendrilDataModel(randFloat(.5, 3.5), randInt(16, 24), new Vector2(.1, .4), new Vector2(4, 6));
+
+    const posX = randFloat(100, 400);
+    const posY = randFloat(50, 125);
+    const posZ = 0;
+    if (imageMaps[i].hasTendrils) {
+        protoOrgs[i] = new ProtoOrganism_Single(new Vector3(randFloat(-posX, posX), randFloat(-posY, posY), randFloat(-posZ, posZ)), vSurf, new Color(randFloat(.3, .7), randFloat(.3, .7), randFloat(.3, .7)), pPhys, tdm);
+    } else {
+        protoOrgs[i] = new ProtoOrganism_Single(new Vector3(randFloat(-posX, posX), randFloat(-posY, posY), randFloat(-posZ, posZ)), vSurf, new Color(.7, .6, .6), pPhys);
+    }
+
+}
+const pp = new ProtoPlasm(protoOrgs, 100, new Vector3(1400, 900, 100));
+pp.setJitter(new Vector3(.1, .1, .1));
+pp.start();
+scene.add(pp);
+
+
+// background
+let env = new VerletPlane2(8200, 3500, 30, 30, "data/ProtoMorph/Proto_BG_006.png", AnchorPlane.EDGES_ALL);
+env.position.setZ(-1900);
+env.moveNode(50, new Vector3(randFloat(30, 60), randFloat(30, 60), randFloat(30, 60)));
+scene.add(env);
+env.renderVerletGeometry(false, false);
+env.setNodesOff(AnchorPlane.EDGES_ALL);
+
+
+/************************************************************/
+
+
+const ambientTexturesLight = new AmbientLight(new Color(randFloat(.75, .9), randFloat(.75, .9), randFloat(.75, .9)), randFloat(.01, .1));
+scene.add(ambientTexturesLight);
+
+const hemiLt = new HemisphereLight(new Color(randFloat(.5, 1), randFloat(.5, 1), randFloat(.5, 1)), new Color(randFloat(.5, 1), randFloat(.5, 1), randFloat(.5, 1)), randFloat(.01, .3));
+scene.add(hemiLt);
+
+const col2 = new Color(1, 1, 1);
+const intensity = .9;
+const light = new DirectionalLight(col2, intensity);
+light.position.set(0, 0, 600);
+light.castShadow = true;
+scene.add(light);
+
+const spot = new SpotLight(new Color(randFloat(.5, 1), randFloat(.5, 1), randFloat(.5, 1)), randFloat(.5, .5));
+spot.position.set(randFloat(-10, 10), randFloat(50, 160), randFloat(500, 550));
+spot.castShadow = true;
+spot.shadow.radius = 12; //doesn't work with PCFsoftshadows
+spot.shadow.bias = -0.0001;
+spot.shadow.mapSize.width = 1024 * 4;
+spot.shadow.mapSize.height = 1024 * 4;
+scene.add(spot);
+
+const pointLt = new PointLight(new Color(randFloat(.3, 1), randFloat(.3, 1), randFloat(.3, 1)), randFloat(.5, 1.2), randFloat(4000, 4000));
+pointLt.translateX(randFloat(0, 0));
+pointLt.translateY(randFloat(0, 0));
+pointLt.translateZ(randFloat(0, 0));
+pointLt.castShadow = true;
+scene.add(pointLt);
+
+// creates spotlight on floor
+const pointLt2 = new PointLight(new Color(randFloat(.8, 1), randFloat(.8, 1), randFloat(.8, 1)), randFloat(.8, 1.5), randFloat(3000, 3000));
+pointLt2.translateX(randFloat(0, 0));
+pointLt2.translateY(randFloat(0, 0));
+pointLt2.translateZ(800);
+pointLt2.castShadow = true;
+//scene.add(pointLt2);
+
+
+function animate() {
+    requestAnimationFrame(animate);
+    controls.update();
+    //controls.autoRotate = true;
+    const time = Date.now();
+
+    /************************Custom code*************************/
+
+    pp.run(time);
+
+    env.verlet();
+    env.jitterNodes(new Vector2(-2, 2));
+    /************************************************************/
+
+
+    render();
+}
+
+function render() {
+    renderer.render(scene, camera);
+}
+animate();
+
+window.addEventListener("resize", onWindowResize)
+function onWindowResize() {
+    (camera.aspect = window.innerWidth / window.innerHeight),
+        camera.updateProjectionMatrix(),
+        renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+window.addEventListener('keydown', (event) => {
+    if (event.key == 'p') {
+        const uid = new Date().getTime();
+        saveImage(renderer, scene, camera, "[Proto]morphogenesis" + uid, 1, 1);
+    }
+})
+
+
+
+
